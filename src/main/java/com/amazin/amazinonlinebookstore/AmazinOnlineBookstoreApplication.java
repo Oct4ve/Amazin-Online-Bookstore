0de9e196f4c746a67f92f8daaca1e6cbb615e4b2
Sam <samwilson292929@gmail.com>
2024-11-07 21:09:47 -0500

    First commit

---
+++ src/main/java/com/amazin/amazinonlinebookstore/AmazinOnlineBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.amazin.amazinonlinebookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazinOnlineBookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmazinOnlineBookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amazin/amazinonlinebookstore/AmazinOnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amazin.amazinonlinebookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazinOnlineBookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
